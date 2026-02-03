629fc7510577e2456f98df142603fb158306384c
CasRepoClone <ccamnorth@gmail.com>
2026-02-03 11:46:21 +0000

    implemented autonomous AI no human in the loop

--- GenAttacks/AI_decisions.py
+++ GenAttacks/AI_decisions.py
@@ -12,37 +12,33 @@ import Agent # change to from . import Agent when implemented outside testing
 
 class Artificial_penetrator():
     def __init__(self):
-        pass
-    def _gen(self, stdout="") -> bool:
-        # make sure filename is not empty
-        """
-        Docstring for generate_exploit
+        self.log = []
+        self.privilageLevel = ""
+    def _gen(self, target="", stdout=""):  
+        clean_response = ""
+        while "DONE" not in clean_response:
+                
+            #region AI-Guardrails
+            message = """You are a penetration tester that can run any scanners or exploits on kali linux using default packages. your job is to gather as much information as possible
+            If you output anything other than commands such as nmap -sV example.com or install commands such as apt get, pip, the response is invalid.
+            Rules:
+            - Never include <think>, </think>, or any reasoning text.
+            - once you have completed all scanning to find vulnerabilities you must place the keywork which is DONE somewhere in your response
+            """
+            #endregion
+            
+            # fill out our prompt
+            out = self.log[-1]
+            if "meow" in clean_response:
+                """if the AI is slightly confused it can get its own log"""
+                out = str(self.log)
+
+            prompt = f"""your current target is still {target} your current last output was {out} if you want to see your full output logs put the word meow in your response""" # return last message with index -1
+            clean_response = Agent.instanciate_agent(message, prompt, strip_markup=True) # check strip markup
+            self.log.append(clean_response)
+        print("scanning complete log available")
         
-        :param cve_id: Identifying value for database of common vulnerability and exposure (CVE)
-        :param target: Target IPv4 address
-        :param maxRetries: Limit for how many times the AI can fail to attack (do not increase unless you want to lose money)
-        :param filename: DO NOT CHANGE
-        :param generate_type: you can change this to scanner or exploit or whatever your heart desires 
-        :return: bool
-        :rtype: bool
-        """
+    def _get_log(self) -> list:
+        return self.log
         
-        #region AI-Guardrails
-        message = """You are a robot that takes a input stream from a terminal and returns a list of discovered vulnerabilities using CVE ids e.g. CVE-012-4243, CVE-015-2342
-        If you output anything other than comma separated variables, the response is invalid.
-        if you are given a vulnerability without a cve labeled you are to discover the matching CVE and add it to the list of comma separated variables to return
-        Rules:
-        - Never include <think>, </think>, or any reasoning text.
-        - return comma separated variables e.g. CVE-012-4243, CVE-015-4234
-        - return comma separated variables only in your response"""
-        #endregion
-        
-        # fill out our prompty
-        prompt = f"""Generate a list of comma separated variables of CVE identifiers for the output stream {stdout}"""
-        clean_code = Agent.instanciate_agent(message, prompt, strip_markup=True) # check strip markup
-        
-        print(clean_code.split(','))
-        return clean_code.split(',')
-        
-    def _test_functionality() -> bool:
-        testcase = """"""
+ 
